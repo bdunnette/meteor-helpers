17c91182b2ac71ca0cf2e4ea8186a450fd32ad55
Brian Dunnette <brian@dunnette.us>
2014-12-07 19:20:24 -0600

    adding new collection list view & navbar link

---
+++ collectionList.js
@@ -0,0 +1,13 @@
+Template.MyCollectionList.helpers({
+  myCollection: function() {
+    return MyCollection.find();
+  }
+});
+
+Template.MyCollectionList.events({
+  
+});
+
+Template.MyCollectionList.rendered = function () {
+  
+};
--- collectionTemplate.js
+++ collectionTemplate.js
@@ -1,1 +1,14 @@
 MyCollection = new Mongo.Collection("myCollection");
+
+MyCollectionSchema = new SimpleSchema({
+  title:{type:String},
+  description:{type:String, optional:true}
+});
+
+MyCollection.attachSchema(MyCollectionSchema);
+
+Router.route('/myCollection', function () {
+  this.render('MyCollectionList');
+}, {
+  name: 'MyCollectionList'
+});
